200148e75240be08d397c811b442ef6b50730d43
DetEMagic <anton.marhold@hotmail.com>
2023-09-29 17:31:19 +0200

    open graph meta

--- components/RootLayout.js
+++ components/RootLayout.js
@@ -7,10 +7,13 @@ export default function RootLayout({meta, children}) {
     <>
       <Head>
         <title>{meta.title}</title>
-        <meta name="application-name" content="Week of Winter"/>
+        <meta name="application-name" content="Week of Winter" key="site_name"/>
         <meta name="description" content={meta.description} key="desc"/>
         <meta name="keywords" content={meta.keywords} key="keyword"/>
-        <meta name="og:description" content={meta.description} key="og:desc"/>
+        <meta property="og:site_name" content="Week of Winter" key="og:site_name"/>
+        <meta property="og:title" content={meta.title} key="og:title"/>
+        <meta property="og:description" content={meta.description} key="og:desc"/>
+        <meta property="og:type" content="website" key="og:type"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" key="viewport"/>
         <link rel="shortcut icon" href="/favicon.ico"/>
         {meta.structuredData ? 
